c6fc3acdc12aaecea03f2a37712ce203559a5aab
Mahiboob Makandar <mahiboobmak1@gmai.com>
2026-07-21 11:14:59 +0530

    login page started

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
