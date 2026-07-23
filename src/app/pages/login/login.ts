4f344563c1d697c5b6ac1c6290700f70704dc6dc
Mahiboob Makandar <mahiboobmak1@gmai.com>
2026-07-23 13:09:41 +0530

    Created professional login page with Angular Material and reactive forms

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import {  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators } from '@angular/forms';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,6 +11,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
+import { validate } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-login',
@@ -18,8 +22,33 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatButtonModule,
     MatCheckboxModule,
-    MatIconModule],
+    MatIconModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  hidePassword =  true;
+  loginForm: FormGroup;
+  title = "QuickBill";
+  constructor(private fb: FormBuilder)
+  {
+    this.loginForm = this.fb.group({
+      emial: ['',Validators.required,Validators.required],
+      password: ['',Validators.required],
+      rememberMe: [false]
+    });
+  }
+login()
+{
+  if(this.loginForm.invalid)
+  {
+    this.loginForm.markAllAsTouched();
+    return;
+  }
+
+  console.log(this.loginForm.value);
+  console.log("hello");
+}
+}
